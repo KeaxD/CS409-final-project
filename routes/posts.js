70ac32a5aec2fa5171962bf84c8c000939e34f50
KeaxD <keanuph@gmail.com>
2023-05-18 09:35:30 -1000

    set up routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,13 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 //Getting all posts
+router.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 //Getting one post
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
 
 //Creating one post
+router.post("/", (req, res) => {});
 
 //Updating one post
+router.patch("/:id", (req, res) => {});
 
 //Delete one post
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
