05528f8df0cbcbe1ae72208b72494eb88e51b594
KeaxD <keanuph@gmail.com>
2023-05-18 08:59:38 -1000

    add post middleware

---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+//Getting all posts
+
+//Getting one post
+
+//Creating one post
+
+//Updating one post
+
+//Delete one post
+
+module.exports = router;
