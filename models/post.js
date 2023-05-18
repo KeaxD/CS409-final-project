4c34461d5f4cd3549334633aeb6c28b3043862c1
KeaxD <keanuph@gmail.com>
2023-05-18 10:11:30 -1000

    set up post route

--- models/post.js
+++ models/post.js
@@ -11,7 +11,6 @@ const postSchema = new mongoose.Schema({
   },
   dateCreated: {
     type: Date,
-    required: true,
     default: Date.now,
   },
 });
--- routes/posts.js
+++ routes/posts.js
@@ -18,7 +18,18 @@ router.get("/:id", (req, res) => {
 });
 
 //Creating one post
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const post = new Post({
+    name: req.body.name,
+    userPostId: req.body.userPostId,
+  });
+  try {
+    const newPost = await post.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 //Updating one post
 router.patch("/:id", (req, res) => {});
