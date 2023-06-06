7d9319e9be4c0c8655a9ca77f7a3b02df3e28f77
KeaxD <keanuph@gmail.com>
2023-06-06 10:22:05 -1000

    add styles and font to postcard

--- client/src/components/PostsCard.jsx
+++ client/src/components/PostsCard.jsx
@@ -6,16 +6,17 @@ const PostCard = ({ _id, name, content, author, dateCreated }) => {
   const formattedDate = new Date(dateCreated).toLocaleDateString(undefined, {
     month: "short",
     day: "numeric",
+    year: "numeric",
   });
 
   return (
     <div className="card-container">
       <div>
-        <h3>{name}</h3>
+        <h3 className="content-title">{name}</h3>
       </div>
       <div>
-        <p>{formattedDate}</p>
-        <p>{content}</p>
+        <p className="date">{formattedDate}</p>
+        <p className="content">{content}</p>
       </div>
     </div>
   );
