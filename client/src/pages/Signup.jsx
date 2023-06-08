b8b207c7bf226ceaa066afdc3793d326a5450253
KeaxD <keanuph@gmail.com>
2023-06-07 14:28:57 -1000

    clean up code

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./css/Login.css";
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
@@ -27,12 +26,12 @@ export default function Signup() {
         if (response.status === 409) {
           console.log("This email is already taken");
         } else if (response.ok) {
-          console.log(`Success! User: ${response.json(user)} was added`);
+          console.log(`Success! User was added`);
         } else {
           console.log("Internal Server Error");
         }
       } catch (e) {
-        alert(e);
+        console.log(e);
       }
     } else {
       alert("Please enter username, email and password");
